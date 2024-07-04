d13aacba2f44c5a9b48dc94dbdca1502286d5cc3
hrpnx <hrpnx@proton.me>
2024-07-04 17:49:20 +0900

    feat: allow adjusting RimShade values in the Menu Installer

--- Editor/MenuItem.cs
+++ Editor/MenuItem.cs
@@ -4,9 +4,16 @@ using VRC.SDKBase;
 
 namespace dev.hrpnx.rim_shade_menu_installer.plugin
 {
-    public class RimShadeMenuInstaller : nadena.dev.modular_avatar.core.AvatarTagComponent { }
+    public class RimShadeMenuInstaller : MonoBehaviour, IEditorOnly
+    {
+        public Color Color;
+        public float NormalStrength;
+        public float Border;
+        public float Blur;
+        public float FresnelPower;
+    }
 
-    public class MenuItem : MonoBehaviour, IEditorOnly
+    public class MenuItem : MonoBehaviour
     {
         [UnityEditor.MenuItem("GameObject/Create RimShadeMenuInstaller", false, 0)]
         public static void Create()
@@ -20,7 +27,12 @@ namespace dev.hrpnx.rim_shade_menu_installer.plugin
 
             var menuInstaller = new GameObject(menuInstallerName);
             menuInstaller.transform.SetParent(avatarRoot.transform);
-            menuInstaller.AddComponent<RimShadeMenuInstaller>();
+            var component = menuInstaller.AddComponent<RimShadeMenuInstaller>();
+            component.Color = new Color(0.5f, 0.5f, 0.5f, 1);
+            component.NormalStrength = 1.0f;
+            component.Border = 0.5f;
+            component.Blur = 1.0f;
+            component.FresnelPower = 1.0f;
         }
     }
 }
