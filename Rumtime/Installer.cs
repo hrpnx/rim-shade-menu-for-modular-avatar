9402571eb3d95fb15cd66fc276410983e3485e93
hrpnx <119768575+hrpnx@users.noreply.github.com>
2024-07-07 15:00:00 +0900

    fix: compile error (#9)

---
+++ Rumtime/Installer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace dev.hrpnx.rim_shade_menu_for_modular_avatar.runtime
+{
+    public class Installer : MonoBehaviour, IEditorOnly
+    {
+        public Color Color;
+        public float NormalStrength;
+        public float Border;
+        public float Blur;
+        public float FresnelPower;
+    }
+}
