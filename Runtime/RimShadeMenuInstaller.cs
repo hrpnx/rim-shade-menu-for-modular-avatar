ef8ef88cdd8b1cd7da984635a3e3e3bd7898f5f9
Corone <flanbel9@gmail.com>
2024-10-14 19:57:41 +0900

    Feature/color exclusions material (#18)
    * マテリアルのカラーだけ除外する指定追加。
    
    * RootMenuのSubMenuに追加する処理追加。

--- Runtime/RimShadeMenuInstaller.cs
+++ Runtime/RimShadeMenuInstaller.cs
@@ -1,10 +1,13 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
+using VRC.SDK3.Avatars.ScriptableObjects;
 using VRC.SDKBase;
 
 namespace dev.hrpnx.rim_shade_menu_for_modular_avatar.runtime
 {
     public class RimShadeMenuInstaller : MonoBehaviour, IEditorOnly
     {
+        public List<Material> ColorExclusions;
         public Color Color;
         public float NormalStrength;
         public float Border;
@@ -12,5 +15,6 @@ namespace dev.hrpnx.rim_shade_menu_for_modular_avatar.runtime
         public float FresnelPower;
         public bool Default;
         public bool Saved;
+        public VRCExpressionsMenu RootMenu;
     }
 }
